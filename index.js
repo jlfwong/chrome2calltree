9046efcb349f78626a49301b690327b70da3b35f
Alexander St Laurent <awstlaur@cs.brown.edu>
2016-01-15 15:10:43 -0500

    general efficiency improvements

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var _s = require("underscore.string");
 
 // Based on WebInspector.CPUProfileView in CPUProfileView.js in Blink source.
 // https://github.com/yoavweiss/Blink/blob/master/Source/devtools/front_end/CPUProfileView.js
-var totalHitCount = function(node) {
+var totalHitCount = function (node) {
     var result = node.hitCount;
     for (var i = 0; i < node.children.length; i++) {
         result += totalHitCount(node.children[i]);
@@ -13,45 +13,54 @@ var totalHitCount = function(node) {
     return result;
 };
 
-var calculateTimes = function(profile) {    
+var calculateTimesForNode = function (node, samplingInterval) {
+    node.selfTime = node.hitCount * samplingInterval;
+    node.selfHitCount = node.hitCount;
+    var totalHitCount = node.hitCount;
+    for (var i = 0; i < node.children.length; i++) {
+        totalHitCount += calculateTimesForNode(node.children[i], samplingInterval);
+    }
+    node.totalTime = totalHitCount * samplingInterval;
+    node.totalHitCount = totalHitCount;
+    return totalHitCount;
+};
+
+var calculateTimes = function (profile) {    
     profile.totalHitCount = totalHitCount(profile.head);
     profile.totalTime = 1000 * (profile.endTime - profile.startTime);
-
     var samplingInterval = profile.totalTime / profile.totalHitCount;
 
-    var calculateTimesForNode = function(node) {
-        node.selfTime = node.hitCount * samplingInterval;
-        node.selfHitCount = node.hitCount;
-        var totalHitCount = node.hitCount;
-        for (var i = 0; i < node.children.length; i++) {
-            totalHitCount += calculateTimesForNode(node.children[i]);
-        }
-        node.totalTime = totalHitCount * samplingInterval;
-        node.totalHitCount = totalHitCount;
-        return totalHitCount;
-    };
-    calculateTimesForNode(profile.head);
+    calculateTimesForNode(profile.head, samplingInterval);
 };
 
-var walkTree = function(node, cb) {
-    if (!node) {
-        return;
+var treeToArrayAcc = function (node, acc) {
+    acc.push(node);
+    for (var i = 0; i < node.children.length; i++) {
+        acc = acc.concat(treeToArrayAcc(node.children[i], []));
     }
-    cb(node);
-    if (!node.children) {
-        return;
-    }
-    node.children.forEach(function(child) {
-        walkTree(child, cb);
-    });
+    return acc;
+}
+
+var treeToArray = function (node) {
+    return treeToArrayAcc(node, []);
+}
+
+var fnForCall = function(call) {
+    var baseUrl = _.last(_.first(call.url.split("?")).split("/"));
+    return _s.sprintf("%s %s:%d",
+            call.functionName, baseUrl, call.lineNumber);
 };
 
-var chromeProfileToCallgrind = function(profile, outStream) {
-    var timedProfile = profile; //_.cloneDeep(profile);
-    calculateTimes(timedProfile);
+var chromeProfileToCallgrind = function (profile, outStream) {
+
+    calculateTimes(profile);
 
     var calls = {};
-    walkTree(timedProfile.head, function(node) {
+
+    var allNodes = treeToArray(profile.head);
+    for (var i = 0; i < allNodes.length; i++) {
+        var node = allNodes[i];
+
         var call = calls[node.callUID] = calls[node.callUID] || {
             functionName: node.functionName,
             url: node.url,
@@ -64,46 +73,42 @@ var chromeProfileToCallgrind = function(profile, outStream) {
         call.selfTime += node.selfTime;
 
         var childCalls = call.childCalls;
-        if (node.children) {
-            node.children.forEach(function(child) {
-                var childUID = child.callUID;
-                var childCall = childCalls[childUID] = childCalls[childUID] || {
-                    functionName: child.functionName,
-                    url: child.url,
-                    totalHitCount: 0,
-                    totalTime: 0,
-                    lineNumber: child.lineNumber
-                };
-                childCall.totalHitCount += child.totalHitCount;
-                childCall.totalTime += child.totalTime;
-            });
+        for(var j = 0; j < node.children.length; j++) {
+            var child = node.children[j];
+
+            var childUID = child.callUID;
+            var childCall = childCalls[childUID] = childCalls[childUID] || {
+                functionName: child.functionName,
+                url: child.url,
+                totalHitCount: 0,
+                totalTime: 0,
+                lineNumber: child.lineNumber
+            };
+            childCall.totalHitCount += child.totalHitCount;
+            childCall.totalTime += child.totalTime;
         }
-    });
+    }
 
     outStream.write('events: ms hits\n');
     outStream.write(_s.sprintf('summary: %d %d\n',
-            timedProfile.totalTime, timedProfile.totalHitCount));
+            profile.totalTime, profile.totalHitCount));
 
-    var fnForCall = function(call) {
-        var baseUrl = _.last(_.first(call.url.split("?")).split("/"));
-        return _s.sprintf("%s %s:%d",
-                call.functionName, baseUrl, call.lineNumber);
-    };
 
-    for (var callUID in calls) {
-        if (!calls.hasOwnProperty(callUID)) {
-            continue;
-        }
-        var call = calls[callUID];
+    // by using Object.keys, we can skip the
+    // hasOwnProperty check
+    var callUIDArray = Object.keys(calls);
+    for (var i = 0; i < callUIDArray.length; i++) {
+        var call = calls[callUIDArray[i]];
+
         outStream.write(_s.sprintf('fl=%s\n', call.url));
         outStream.write(_s.sprintf('fn=%s\n', fnForCall(call)));
         outStream.write(_s.sprintf('%d %d %d\n', call.lineNumber,
                 call.selfTime, call.selfHitCount));
-        for (var childCallUID in call.childCalls) {
-            if (!call.childCalls.hasOwnProperty(childCallUID)) {
-                continue;
-            }
-            var childCall = call.childCalls[childCallUID];
+
+        var childCallUIDArray = Object.keys(call.childCalls);
+        for (var j = 0; j < childCallUIDArray.length; j++) {
+            var childCall = call.childCalls[childCallUIDArray[j]];
+
             outStream.write(_s.sprintf('cfi=%s\n', childCall.url));
             outStream.write(_s.sprintf('cfn=%s\n', fnForCall(childCall)));
 
